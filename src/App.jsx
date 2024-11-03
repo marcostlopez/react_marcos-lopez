e69d57bad7a36bfaab41305f0d70b48eaa1de800
marcos lopez <marcos_mtil@hotmail.com>
2024-11-02 23:59:16 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './NavBar'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Navbar from './NavBar';
 
 function App() {
   const [count, setCount] = useState(0)
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,12 @@
-import React from 'react';
-import './index.css';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="logo">
-                <Link>Ecommerce</Link>
-            </div>
-            <ul className="nav-links">
-                <li>
-                    <Link>Inicio</Link>
-                </li>
-                <li>
-                    <Link>Productos</Link>
-                </li>
-                <li>
-                    <Link>Carrito</Link>
-                </li>
-                <li>
-                    <Link>Contacto</Link>
-                </li>
-            </ul>
+const Navbar = () =>{
+    return(
+        <nav>
+            <a>inicio</a>
+            <a>ofertas</a>
+            <a>productos</a>
+            <a>carrito</a>
         </nav>
-    );
-};
-
-export default Navbar;
+    )
+}
+export default Navbar
